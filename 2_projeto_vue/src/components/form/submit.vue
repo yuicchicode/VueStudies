84817770bdfd25f280bb4be9dd27b5144581c3db
yuicchicode <Yuicchisama@gmail.com>
2023-04-16 16:39:42 -0300

    hierarchy0.0.1

---
+++ 2_projeto_vue/src/components/form/submit.vue
@@ -0,0 +1,9 @@
+<template>
+    <input type="submit" value="Send">
+</template>
+
+<script>
+export default {
+    name: 'submit'
+}
+</script>
--- 2_projeto_vue/src/components/person.vue
+++ 2_projeto_vue/src/components/person.vue
@@ -2,20 +2,24 @@
     <div>
         <h2>Esta é a descrição da pessoa: {{ nome }}</h2>
         <info/>
+        <Form/>
     </div>
 </template>
 
 <script>
 import info from './info.vue';
+import Form from './Form.vue';
+
     export default {
         name: 'person',
         components: {
-            info
+            info,
+            Form
         },
         data() {
             return {
                 nome: 'David'
             }
-        },   
+        }
     }
 </script>
