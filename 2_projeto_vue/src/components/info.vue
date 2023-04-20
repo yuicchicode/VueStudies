11ef128ac9ef0bb3ba077edfd826d894a460faec
yuicchicode <Yuicchisama@gmail.com>
2023-04-19 21:37:28 -0300

    if for show

--- 2_projeto_vue/src/components/info.vue
+++ 2_projeto_vue/src/components/info.vue
@@ -1,7 +1,8 @@
 <template>
 
     <div>
-        <p> Estou trabalhando no momento. </p>
+        <p v-if="esta_trabalhando"> Estou trabalhando no momento. </p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias: </p>
         <ul>
             <li>JavaScript</li>
@@ -9,6 +10,7 @@
             <li>Vue.js</li>
             <li>Node.js</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 
 </template>
@@ -17,7 +19,14 @@
 <script>
 
     export default {
-    name: 'info' 
+    name: 'info' ,
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: "teste@gmail.com"
+        }
+    }
     }
 
 </script>
