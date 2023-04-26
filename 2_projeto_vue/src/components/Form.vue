69ec8da9012e5bb1b43fbde23a4e0cebb0b1bf1c
yuicchicode <Yuicchisama@gmail.com>
2023-04-25 22:38:31 -0300

    submit and click

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <h2>Meu formulário</h2>
-        <form action="">
+        <form action="" @submit="enviarFormulario($event)">
             <div>
-                <inputText/>
+                <input type="text" v-model="name">
             </div>
             <div>
-                <inputText/>
+                <input type="text" v-model="email">
             </div>
             <div>
                 <submit/>
@@ -24,6 +24,24 @@ import submit from './form/submit.vue';
         components: {
             inputText,
             submit
+        },
+        data() {
+            return {
+                name: "",
+                email:""
+            }
+        },
+        methods: {
+            enviarFormulario(e) {
+                e.preventDefault();
+                const name = this.name;
+                const email = this.email;
+            
+
+                console.log("Formulário Enviado!")
+                console.log("O nome é " + name)
+                console.log("O email é " + email)
+            }
         }
     }
 </script>
