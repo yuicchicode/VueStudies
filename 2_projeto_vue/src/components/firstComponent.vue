db79b6c18560516649fbea077039dfbe0f3d5780
yuicchicode <Yuicchisama@gmail.com>
2023-04-26 23:24:17 -0300

    Events

--- 2_projeto_vue/src/components/firstComponent.vue
+++ 2_projeto_vue/src/components/firstComponent.vue
@@ -2,13 +2,18 @@
     <div>
         <h1>Opa Vue!</h1>
         <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+        <MultiplosEventos/>
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
 
     export default {
         name: 'firstComponent',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: "David",
