94a93abb9b552fcaf56f8429dd784ef3ce5b1f99
yuicchicode <Yuicchisama@gmail.com>
2023-05-03 23:24:05 -0300

    Form complete

--- app_project/src/components/BurgerForm.vue
+++ app_project/src/components/BurgerForm.vue
@@ -1,11 +1,47 @@
 <template>
     <div>
         <p>Componente de Mensagem</p>
-        <div>
-            <form id="burger-fomr">
+        <div id="burger-form-container">
+            <form id="burger-form">
                 <div class="input-container">
-                    <label for="nome">Nome do cliente</label>
+                    <label for="nome">Nome do cliente:</label>
                     <input type="text" id="nome" name="name" v-model="nome" placeholder="Digite o seu nome">
+                </div>
+                <div class="input-container">
+                    <label for="pao">Escolha o pão:</label>
+                    <select name="pao" id="pao">
+                        <option value="">Selecione o seu pão</option>
+                        <option value="integral">integral</option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="carne">Escolha o pão:</label>
+                    <select name="carne" id="carne">
+                        <option value="">Selecione a sua carne</option>
+                        <option value="Picanha">Picanha</option>
+                    </select>
+                </div>
+                <div id="opcionais-container" class="input-container">
+                    <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>salame</span>
+                    </div>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>salame</span>
+                    </div>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>salame</span>
+                    </div>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>salame</span>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <input type="submittext" class="submit-btn" value="Criar meu Burger">
                 </div>
             </form>
         </div>
@@ -17,3 +53,78 @@ export default {
     name: "BurgerForm"
 }
 </script>
+
+<style scoped>
+    #burger-form-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    #burger-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select {
+        padding: 5px 10px;
+        width: 300px
+    }
+
+    #opcionais-container {
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title {
+        width: 100%;
+    }
+
+    .checkbox-container {
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span,
+    .checkbox-container input {
+        width: auto;
+    }
+
+    .checkbox-container span {
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn {
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        text-align: center;
+        cursor: pointer;
+        transition: .5s;
+    }
+.submit-btn:hover {
+    background-color: transparent;
+    color: #222;
+}
+
+</style>
