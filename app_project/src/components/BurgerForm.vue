2b3bf1ecb7bd488143d58cd744d63f1b98fb49a6
yuicchicode <Yuicchisama@gmail.com>
2023-05-03 22:52:50 -0300

    Form update

---
+++ app_project/src/components/BurgerForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Componente de Mensagem</p>
+        <div>
+            <form id="burger-fomr">
+                <div class="input-container">
+                    <label for="nome">Nome do cliente</label>
+                    <input type="text" id="nome" name="name" v-model="nome" placeholder="Digite o seu nome">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BurgerForm"
+}
+</script>
